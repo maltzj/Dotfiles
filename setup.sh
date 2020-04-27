8c0714deb9f93f22b444b8c1638aabfaa7ec5ba1
Jonathan Maltz <maltz@stripe.com>
2020-04-27 11:37:37 -0700

    cleaned up gitconfig to make it work with work laptop

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ curl https://pyenv.run | zsh
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+# oh-my-zsh will create a zshrc, but we don't want that one.
+mv ~/.zshrc ~/.zshrc.backup
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 git submodule update --init
