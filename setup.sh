589f004f80506c9cf17f23e71e2ede5f5526dd49
Jonathan Maltz <maltz@stripe.com>
2020-05-01 18:03:52 -0700

    new aliases

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv ~/.zshrc ~/.zshrc.backup
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
 curl https://pyenv.run | zsh 
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 # oh-my-zsh will create a zshrc, but we don't want that one.
-mv ~/.zshrc ~/.zshrc.backup
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+
 git submodule update --init
